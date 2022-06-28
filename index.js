819dd450a86d1ecf2c9e8222218123fdf7616413
LucasSVA <lucas.svanadze@gmail.com>
2022-06-28 19:51:52 +0200

    exercise 1 done

---
+++ index.js
@@ -0,0 +1,14 @@
+// ---- ex 1 
+
+// const sortLetters = (str) => {
+
+//     let wordSplit = str.split("")
+//     wordSplit.sort();
+//     let wordJoin = wordSplit.join("");
+//     return wordJoin;
+
+// }
+
+
+// console.log(sortLetters("konexio"));
+
