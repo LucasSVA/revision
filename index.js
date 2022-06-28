875e7a73dbacee340d018ef0050df36456820926
LucasSVA <lucas.svanadze@gmail.com>
2022-06-28 23:58:08 +0200

    exercice 2 done

--- index.js
+++ index.js
@@ -12,3 +12,33 @@
 
 // console.log(sortLetters("konexio"));
 
+
+
+// --- ex 2 
+
+// const countEach = (str) => {
+//     let letterX = 0
+//     let letterO = 0
+//     let strSplit = str.split("")
+
+//     strSplit.forEach(element => {
+    
+//     if (element === "x") {
+//         letterX += 1
+        
+//     }else if (element === "o") {
+//         letterO += 1
+//     }
+
+//     });
+//     //console.log(letterX, letterO);
+
+//     if (letterX === letterO) {
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+
+// }
+// countEach("xxxoooox")
+// countEach("xox")
